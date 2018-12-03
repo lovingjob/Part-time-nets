bfea639f932de5d0e82e567db9a8d42d98fec6aa
Administrator <Administrator@USER-20180120BT>
2018-12-03 18:21:25 +0800

    1

---
+++ src/com/servlet/TestServlet.java
@@ -0,0 +1,22 @@
+package com.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+public class TestServlet extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("我是servlet.");
+	}
+}
